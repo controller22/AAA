5012f235bd9e83d7e1e991a0d77165a421c4f1fd
DH <shim5013@naver.com>
2023-02-10 20:19:59 +0900

    시작합시다

---
+++ src/main/java/shop/mtcoing/sadasjbj/SadasjbjApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoing.sadasjbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadasjbjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadasjbjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoing/sadasjbj/SadasjbjApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoing.sadasjbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadasjbjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
